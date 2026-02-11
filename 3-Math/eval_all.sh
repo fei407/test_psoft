5bf25a83dc01ece3c36e231771707bcfd2f8aa74
Fei Wu <fw407@exeter.ac.uk>
2026-02-11 10:37:11 +0000

    init proj

--- 3-Math/eval_all.sh
+++ 3-Math/eval_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python eval_gsm8k.py --model './results/metamath40k/Llama-3.2-3B/H100_PSOFT_r168_peft_lr-4e-4/' --data_file ./data/test/GSM8K_test.jsonl
-python eval_math.py --model './results/metamath40k/Llama-3.2-3B/H100_PSFOT_r168_peft_lr-4e-4/' --data_file ./data/test/MATH_test.jsonl
+python eval_gsm8k.py --model './results/metamath40k/Llama-3.2-3B/H100_PSOFT_r256/' --data_file ./data/test/GSM8K_test.jsonl
+python eval_math.py --model './results/metamath40k/Llama-3.2-3B/H100_PSFOT_r256/' --data_file ./data/test/MATH_test.jsonl
 
